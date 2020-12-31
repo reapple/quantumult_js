2f278242df34d29962b06db53b81e031575b4821
reapple <45746666+reapple@users.noreply.github.com>
2020-12-31 11:51:55 +0800

    Update thhz.js

--- thhz.js
+++ thhz.js
@@ -1,10 +1,11 @@
 let obj = JSON.parse($response.body);
-console.log(obj);
+
 let time = "1924907841";
+
 obj.code = "1";
-console.log(obj.code);
+
 obj.viptime = time;
-console.log(obj.viptime);
+
 obj.msg.viptime = time;
-console.log(obj.msg.viptime);
+
 $done({ body: JSON.stringify(obj) });
