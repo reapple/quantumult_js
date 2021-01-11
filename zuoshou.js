7d257ab2f3bd18d426d7dc77f2be2bf296a73551
reapple <45746666+reapple@users.noreply.github.com>
2021-01-11 16:23:32 +0800

    Update zuoshou.js

--- zuoshou.js
+++ zuoshou.js
@@ -4,14 +4,12 @@ let count = 9999;
 
 if (obj.hasOwnProperty("data")) {
     if (obj.data.hasOwnProperty("views")) {
-        obj.data.total = count;
-        obj.data.remaining = 0;
+        obj.data.views.total = count;
+        obj.data.views.remaining = 0;
     }
-}
-if (obj.hasOwnProperty("grade")) {
-    if (obj.grade.hasOwnProperty("current")) {
-        obj.grade.title = "最强王者";
-        obj.grade.grade = 5;
+    if (obj.data.hasOwnProperty("grade")) {
+        obj.data.grade.title = "最强王者";
+        obj.data.grade.grade = 5;
     }
 }
 
