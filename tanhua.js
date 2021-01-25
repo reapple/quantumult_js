b7ff6a09d35b9e3b73f141dc6859a160b10f12b7
reapple <45746666+reapple@users.noreply.github.com>
2021-01-26 07:57:00 +0800

    Update tanhua.js

--- tanhua.js
+++ tanhua.js
@@ -1,13 +1,15 @@
-var body = $response.body;
+body = {
+  "status" : "200",
+  "msg" : "返回成功",
+  "data" : {
+    "mobile" : "18866889999",
+    "status" : 1,
+    "is_vip" : 1,
+    "member_name" : "作者TG@iCodess",
+    "head_pic" : "https://s3.ax1x.com/2021/01/26/sOLQKA.jpg",
+    "parent_id" : 22,
+    "user_viptime" : "2066-06-06"
+  }
+};
 
-var obj = JSON.parse(body);
-
-obj.data.is_vip = 1;
-obj.data.user_viptime = "2066-06-06";
-obj.data.member_name = "作者TG@iCodess";
-
-body = JSON.stringify(obj);
-
-console.log(body);
-
-$done(body);
+$done(JSON.stringify(body));
