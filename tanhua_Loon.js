04f52832f8b1a14dfe7d94746484351038231a46
reapple <45746666+reapple@users.noreply.github.com>
2021-01-26 14:22:59 +0800

    Create tanhua_Loon.js

---
+++ tanhua_Loon.js
@@ -0,0 +1,15 @@
+var body = {
+  "status" : "200",
+  "msg" : "返回成功",
+  "data" : {
+    "mobile" : "18866889999",
+    "status" : 1,
+    "is_vip" : 1,
+    "member_name" : "作者TG@iCodess",
+    "head_pic" : "https://s3.ax1x.com/2021/01/26/sOLQKA.jpg",
+    "parent_id" : 22,
+    "user_viptime" : "2066-06-06"
+  }
+};
+
+$done({body});
